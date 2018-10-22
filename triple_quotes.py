673f8f6ed916b059e1c8afdbf9a83efb8dc3087d
rosalieper <rosalieperside@gmail.com>
2018-10-22 05:55:21 +0100

    triple strings

---
+++ triple_quotes.py
@@ -0,0 +1,9 @@
+#!urs/bin/python
+
+para_string = """ This string is a paragraph
+string It will apear just the way it was typed with all the new
+lines in the output.
+Enjoy python :)
+"""
+
+print para_string;
