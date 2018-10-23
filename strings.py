4612dee78389d714f007aa92b168035f5bf4283b
rosalieper <rosalieperside@gmail.com>
2018-10-23 06:49:20 +0100

    string examples

---
+++ strings.py
@@ -0,0 +1,9 @@
+#!urs/bin/python
+
+str = "this is an example string...WoW!!!";
+
+print "str.capitalize(): ", str.capitalize()
+print "str.center(40, 'a'): ", str.center(40, 'a')
+sub = "i"
+print "str.count(sub, 4, 40): ", str.count(sub, 4, 40)
+print "str.encode(base64, 'strict')", str.encode('base64', 'strict')
