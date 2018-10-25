ba095c81fe9a7f59c7f02e1effe0a6642cc7c7ef
rosalieper <rosalieperside@gmail.com>
2018-10-25 08:21:39 +0100

    manipulating lists

---
+++ deleting_in_list.py
@@ -0,0 +1,20 @@
+#!usr/bin/python
+
+list1 =['python', 'physics', 'chemistry', '1550', 2018]
+
+print list1
+
+del list1[3]
+
+print "After deleting value of index 3: "
+print list1
+
+print "length: ", len(list1)
+
+print "list*2: ", list1*2
+
+print "python in list: ", "python" in list1
+
+print "List: "
+
+for x in list1: print x
