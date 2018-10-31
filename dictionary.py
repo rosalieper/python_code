7093ce5f982e9c736e6494527e89b6870dc6f529
rosalieper <rosalieperside@gmail.com>
2018-10-31 22:12:28 +0100

    behaviors of keys in dictionaries in python

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,7 @@
 dict = {'Name': 'Zara', 'Age': 7, 'Name': 'Manni'};
 
 print "dict['Name']: ", dict['Name'];
+
+#this will yield an error
+dict = {['Name']: 'Zara', 'Age': 7};
+print "dict['Name']: ", dict['Name'];
