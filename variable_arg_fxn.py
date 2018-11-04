9981455d4d28e3a08d6d1735e761e4fd14230737
rosalieper <rosalieperside@gmail.com>
2018-11-04 23:58:22 +0100

    manipulation functions and variable arguments in python

---
+++ variable_arg_fxn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Function definition is here
+
+def printinfo( arg1, *vartuple ):
+	"This prints a variable passed arguments"
+	print "Output is: "
+	print arg1
+	for var in vartuple:
+	print var
+	return;
+
+# Now you can call printinfo function
+
+printinfo( 10 );
+printinfo( 70, 60, 50 );
