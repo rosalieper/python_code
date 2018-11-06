d85081e7b9b3b9fe799b5fbc2efa979c9887ac19
rosalieper <rosalieperside@gmail.com>
2018-11-06 20:48:26 +0100

    anonymous functions

---
+++ lamda_functions.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# anonymous Function definition is here
+sum = lambda arg1, arg2: arg1 + arg2;
+
+# Now you can call sum as a function
+print ("Value of total : ", sum( 10, 20 ))
