63e2c5d33930bd0079026abd09b3b7a57e4262a8
rosalieper <rosalieperside@gmail.com>
2018-11-06 21:29:32 +0100

    using modules and importing

---
+++ import_modules.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# Import module module_print_fxn
+import module_print_fxn
+
+# Now you can call defined function that module as follows
+module_print_fxn.print_func("Zara")
--- lamda_functions.py
+++ lamda_functions.py
@@ -4,4 +4,4 @@
 sum = lambda arg1, arg2: arg1 + arg2;
 
 # Now you can call sum as a function
-print ("Value of total : ", sum( 10, 20 ))
+print ("Value of total : "), sum( 10, 20 )
