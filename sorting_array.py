96cc8ea568c15ef5cc2171b3440c47b6a20c037c
rosalieper <rosalieperside@gmail.com>
2018-11-09 23:23:13 +0100

    andela test2

---
+++ sorting_array.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+states = ["Abia", "Adamawa", "Anambra", "Akwa Ibom", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Enugu", "Edo", "Ekiti", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara"]
